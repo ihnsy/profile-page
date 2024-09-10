2e8d621197b362bee143dfd4c94af530c4f81128
ihnsy <snu1127@snu.ac.kr>
2024-09-11 07:35:31 +0900

    배경 영상에서 사진으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,19 @@ import './App.css';
 
 function App() {
   const name = '인소윤';
+  const studentinfo = '건설환경공학부 23학번';
+  const age = '21세';
   const hobbies = ['여행', '베이킹', '발레'];
 
   return (
     <div className="profile-container">
-      {/* YouTube 영상 임베드! */}
-      <div className="background-video">
-        <iframe
-          src="https://youtu.be/_LJ62r7sIj8?si=JQKQuozI4jV5yKux"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="autoplay; fullscreen"
-          allowFullScreen
-        ></iframe>
-      </div>
+      {/* 반투명 배경 이미지 */}
+      <div className="background-image"></div>
 
       <div className="content">
         <h1 className="welcome-message">{name}의 프로필입니다!</h1>
+        <p>{studentinfo}</p>
+        <p>나이: {age}</p>
         <h2>취미: </h2>
         <ul>
           {hobbies.map((hobby, index) => (
